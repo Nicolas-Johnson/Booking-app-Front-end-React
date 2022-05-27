59b1a068a9b13a36930e66be0ce5063d6e4ee96e
Nicolas Johnson da SIlva Sousa <nicolas-sousa2011@hotmail.com>
2022-05-27 20:39:36 -0300

    Home style

--- src/components/propetyLoved/PropertyLovedListItem.js
+++ src/components/propetyLoved/PropertyLovedListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PropertyLovedListItem = ({ price, name, city, rating, avaliation, imgUrl }) => {
   return (
     <div className="lovedListItem">
-      <img src={ imgUrl } alt={ name } className="lovedListItemImg" />
+      <img src={ imgUrl } alt={ name } className="lovedListItemImg" className="lovedListItemImg" />
       <span className="lovedListItem-name">{ name }</span>
       <span className="lovedListItem-city">{ city }</span>
       <span className="lovedListItem-price">Apartir de R${price}</span>
